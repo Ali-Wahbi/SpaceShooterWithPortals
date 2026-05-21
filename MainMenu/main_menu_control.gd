ec12c4ca67db959197dbf45d70e71eda933185c0
Ali-Wahbi <aliwahbi436@gmail.com>
2026-05-21 11:40:38 +0300

    main menu space bg move fix and add position change option

--- MainMenu/main_menu_control.gd
+++ MainMenu/main_menu_control.gd
@@ -8,16 +8,13 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	spaceMouseOffset()
-	pass
 
 #region buttons
 func _on_play_mouse_entered() -> void:
 	hoverSFX.play()
-
 
 func _on_play_pressed() -> void:
 	pressSFX.play()
@@ -26,7 +23,6 @@ func _on_play_pressed() -> void:
 	await get_tree().create_timer(1).timeout
 	# go to main game area
 	get_tree().change_scene_to_file("res://mainGameArea.tscn")
-
 
 func _on_quit_mouse_entered() -> void:
 	hoverSFX.play()
@@ -40,10 +36,10 @@ func _on_quit_pressed() -> void:
 #endregion
 
 #region space
-
 @onready var tileMap: TileMapLayer = $StarsBg
 @export var minimumOffset: Vector2
 @export var maximumTileOffset: Vector2
+
 var xOffset: float:
 	set(value):
 		xOffset = value
@@ -53,19 +49,27 @@ var yOffset: float:
 		yOffset = value
 		setSpaceYOffset()
 
+var prevMousePos: Vector2 = -Vector2(1000,1000)
 func spaceMouseOffset():
 	var mousePos = get_global_mouse_position() / get_viewport_rect().size - Vector2.ONE / 2.0
+	if prevMousePos == mousePos:
+		return
+	prevMousePos = mousePos
 	if abs(mousePos.x) > minimumOffset.x:
 		xOffset = mousePos.x - minimumOffset.x
 	if abs(mousePos.y) > minimumOffset.y:
 		yOffset = mousePos.y - minimumOffset.y
-	pass
 
 func setSpaceXOffset():
 	if tileMap:
+#		Change offset using shader
 		tileMap.material.set_shader_parameter("addX", -xOffset * maximumTileOffset.x)
-
+#		Change offset using position
+		#tileMap.position.x = -xOffset * maximumTileOffset.x
 func setSpaceYOffset():
 	if tileMap:
+#		Change offset using shader
 		tileMap.material.set_shader_parameter("addY", -yOffset * maximumTileOffset.y)
+#		Change offset using position
+		#tileMap.position.y = -yOffset * maximumTileOffset.y
 #endregion
