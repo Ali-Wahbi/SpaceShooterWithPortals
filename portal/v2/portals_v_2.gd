1739f946dc5980fc77fef332425043b4f1e971c2
Ali-Wahbi <aliwahbi436@gmail.com>
2026-05-23 19:28:21 +0300

    portal v2 step2

---
+++ portal/v2/portals_v_2.gd
@@ -0,0 +1,31 @@
+@tool
+extends Node2D
+
+@export var isPart1: bool = true:
+	set(value):
+		isPart1 = value
+		if sideSprite:
+			var anim = "portalSide1" if isPart1 else "portalSide2"
+			sideSprite.animation = anim
+		if collision:
+			if isVertical:
+				collision.rotation_degrees = -90 if isPart1 else 90
+
+			# sideSprite.p
+@export var isVertical: bool = true:
+	set(value):
+		isVertical = value
+		if sideSprite:
+			sideSprite.rotation_degrees = 90 if isVertical else 0
+			
+@export var sideSprite: AnimatedSprite2D
+@export var collision: RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
