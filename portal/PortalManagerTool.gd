cdca8170de54b3555026e25edcbb7d1aeaa7bec0
Ali-Wahbi <aliwahbi436@gmail.com>
2026-05-24 17:25:05 +0300

    portal teleport test one

--- portal/PortalManagerTool.gd
+++ portal/PortalManagerTool.gd
@@ -7,9 +7,11 @@ var portalLabel1: Label
 var portalLabel2: Label
 
 var connectPortalsButton: Button
+var slider: HSlider
 
 var editorSelection: EditorSelection
-
+var windowPos = Vector2(1100, 100)
+var windowSize = Vector2(420, 200)
 # Called when the node enters the scene tree for the first time.
 func _run() -> void:
 	editorSelection = EditorInterface.get_selection()
@@ -18,7 +20,7 @@ func _run() -> void:
 	window = Window.new()
 	window.always_on_top = true
 	window.transient = false
-	EditorInterface.popup_dialog(window, Rect2(Vector2(1100, 100), Vector2(420, 180)))
+	EditorInterface.popup_dialog(window, Rect2(windowPos, windowSize))
 
 	makeGUI()
 	updateLabels()
@@ -29,9 +31,10 @@ func _run() -> void:
 func makeGUI():
 	var hBox = HBoxContainer.new()
 	var vBox = VBoxContainer.new()
-
+	slider = HSlider.new()
 	hBox.custom_minimum_size = Vector2(400, 100)
 	vBox.custom_minimum_size = Vector2(200, 100)
+	slider.custom_minimum_size = Vector2(100, 100)
 	
 
 	portalLabel1 = Label.new()
@@ -48,8 +51,10 @@ func makeGUI():
 	connectPortalsButton.disabled = not editorSelection.get_selected_nodes().size() == 2
 	connectPortalsButton.pressed.connect(onConnectPressed)
 
-
+	slider.value_changed.connect(onSliderChanged)
+	slider.editable = not connectPortalsButton.disabled
 	window.add_child(hBox)
+	window.add_child(slider)
 	hBox.add_child(vBox)
 	hBox.add_child(connectPortalsButton)
 
@@ -57,9 +62,12 @@ func makeGUI():
 	vBox.add_child(portalLabel2)
 	# window.add_child(connectPortalsButton)
 	hBox.position = Vector2(20, 40)
+	slider.position = Vector2(20, 100)
+	
+
 	portalLabel1.text = "Not Selected"
 	portalLabel2.text = "Not Selected"
-
+	slider.value = 50
 func updateLabels():
 	var allNodes = editorSelection.get_selected_nodes()
 
@@ -74,10 +82,12 @@ func updateLabels():
 			portalLabel2.text = "Not Selected"
 		
 		connectPortalsButton.disabled = not editorSelection.get_selected_nodes().size() == 2
+		slider.editable = not connectPortalsButton.disabled
 	else:
 		portalLabel1.text = "Not Selected"
 		portalLabel2.text = "Not Selected"
 		connectPortalsButton.disabled = true
+		slider.editable = not connectPortalsButton.disabled
 	print("Selection Changed")
 
 func onConnectPressed():
@@ -87,3 +97,21 @@ func onConnectPressed():
 
 	portal1.otherPortal = portal2
 	portal2.otherPortal = portal1
+
+func onSliderChanged(value: float):
+	var angleMod = deg_to_rad(30.0)
+	var newValue = value - 50
+	var newAngle = newValue * PI * 2 / 100
+	# newAngle = fmod(newAngle, deg_to_rad(angleMod))
+	newAngle = snappedf(newAngle, angleMod)
+	print(rad_to_deg(newAngle))
+	setPortalRotation(newAngle)
+
+
+func setPortalRotation(value: float):
+	var allNodes = editorSelection.get_selected_nodes()
+	var portal1: Portal = allNodes[0]
+	var portal2: Portal = allNodes[1]
+
+	portal1.rotation = value
+	portal2.rotation = value + PI
